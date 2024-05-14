eb93eef05e03ed1d271d0cc1ca3abf7364837a37
clemencebc1 <143797406+clemencebc1@users.noreply.github.com>
2024-05-14 15:25:52 +0200

    Add files via upload

--- modèle/TestEpreuve.java
+++ modèle/TestEpreuve.java
@@ -2,11 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 
 public class TestEpreuve {
     Sport natation = new Natation("Natation", "Eau");
+    Pays france = new Pays("France");
+    Equipe equipeFrance = new Equipe("France");
     Epreuve epreuveNatation = new Epreuve("100m brasse hommes", "hommes", "individuel", natation);
+    Athlete marchand = new Athlete("Marchand", "Léon", "M", 10, 20, 40, france);
+    Pays angleterre = new Pays("Angleterre");
+
     @Test 
     public void testGetNom(){
         assertEquals(epreuveNatation.getNom(), "100m brasse hommes");
@@ -18,5 +24,11 @@ public class TestEpreuve {
     @Test 
     public void testGetParticipants(){
         assertEquals(epreuveNatation.getCategorie(), "hommes");
+    }
+    @Test 
+    public void testClassement(){
+        Comparator<Athlete> comp1 = new CompareMedailleOr();
+        epreuveNatation.ajoutParticipants(marchand);
+
     }
 }
