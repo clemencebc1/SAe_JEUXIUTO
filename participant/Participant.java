70afa2cb54e5a46b742591e5dd74a0d8ef7c3590
Clemence Bocquet <143797406+clemencebc1@users.noreply.github.com>
2024-06-18 08:46:10 +0200

    Delete participant directory

--- participant/Participant.java
+++
@@ -1,7 +0,0 @@
-package participant;
-import sport.*;
-
-public interface Participant {
-    /** fait participer un althète ou une equipe a une epreuve */
-    public abstract double participer(Epreuve e);
-}
