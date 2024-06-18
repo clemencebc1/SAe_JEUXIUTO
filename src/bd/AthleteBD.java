3cb8833560756b607762ba7d7f386a17415cb40f
MatheoLobjois <143810213+MatheoLobjois@users.noreply.github.com>
2024-06-18 09:13:54 +0200

    Update AthleteBD.java

--- src/bd/AthleteBD.java
+++ src/bd/AthleteBD.java
@@ -13,63 +13,7 @@ public class AthleteBD {
 	public void Athlete(ConnexionMySQL laConnexion){
 		this.laConnexion = laConnexion;
 	}
-	int maxNumAthlete() throws SQLException{
-		this.st = this.laConnexion.createStatement();
-		ResultSet rs = this.st.executeQuery("select max(id_A) from ATHLETE");
-		if (rs.next()){
-			int maxnum = rs.getInt(1);
-			/** int macnum.rs.get("max(numJoueur)") */
-			return maxnum;
-		}
-		return -1;
-	}
-
-	/** C'est mieux que insererJoueur_old */
-	int insererAthlete( participant.Athlete j) throws  SQLException{
-		PreparedStatement ps = this.laConnexion.prepareStatement("insert into ATHLETE(id_A,nom_A,prenom_A,sexe_A,id_P,force_A,endurance,agilite,id_E) values (?,?,?,?,?,?,?,?,?)");
-		int nouvNum = maxNumAthlete()+1;
-		ps.setInt(1, nouvNum);
-		ps.setString(2, j.getNom());
-		ps.setString(3, j.getPrenom());
-		ps.setString(4, j.getSexe());
-		ps.setInt(5, j.getPays().getNum());
-		ps.setInt(6, j.getForce());
-		ps.setInt(7, j.getEndurance());
-		ps.setInt(8, j.getAgilite());
-		ps.setInt(9, j.getEquipe().getNum());
-		ps.executeUpdate();
-		return nouvNum;
-	}
-
-	void insererParticipation(Athlete a, Epreuve e)throws SQLException{
-		PreparedStatement ps = this.laConnexion.prepareStatement("insert into PARTICIPER(id_A,id_Ep) values (?,?)");
-		ps.setInt(1, getNumAthlete(a));
-		ps.setInt(2, getNumEpreuve(e));
-		ps.executeUpdate();
-	}
-
-	int getNumAthlete(Athlete a) throws SQLException{
-		this.st = this.laConnexion.createStatement();
-		ResultSet rs = this.st.executeQuery("SELECT id_A FROM ATHLETE WHERE nom_A = "+ a.getNom() +"AND prenom_A = "+ a.getPrenom()+"AND sexe_A =" + a.getSexe());
-			if (rs.next()){
-				int maxnum = rs.getInt(1);
-
-				return maxnum;
-			}
-			return -1;
-	}
-
-	int getNumEpreuve(Epreuve e)throws SQLException{
-
-		PreparedStatement ps = this.laConnexion.prepareStatement("SELECT id_Ep FROM EPREUVE WHERE nom = "+  e.getNom()+"AND categorie = "+ e.getCategorie()+ "AND id_S = " + e.getSport());
-		ResultSet rs = ps.executeQuery();
-			if (rs.next()){
-				int maxnum = rs.getInt(1);
-
-				return maxnum;
-			}
-			return -1;
-	}
+	
 	public boolean estAdmin(String log, String mdp) throws SQLException{
 		log = "'"+log+"'";
 		mdp = "'"+mdp+"'";
@@ -137,6 +81,121 @@ public class AthleteBD {
 		}
 		rs.close();
 		return listePays;
+	}
+////////////////////////////////////// ADMIN //////////////////////////////////////
+	void effacerAthlete(int num) throws SQLException {
+		Statement st = this.laConnexion.createStatement();
+		st.executeUpdate("delete from ATHLETE where id_A = " + num);
+		st.executeUpdate("delete from PARTICIPER where id_A = " + num);
+	}
+	void effacerEpreuve(int num) throws SQLException {
+		Statement st = this.laConnexion.createStatement();
+		st.executeUpdate("delete from EPREUVE where id_Ep = " + num);
+		st.executeUpdate("delete from PARTICIPER where id_Ep = " + num);
+	}
+
+	void effacerUser(String nom,String mdp) throws SQLException {
+		Statement st = this.laConnexion.createStatement();
+		st.executeUpdate("delete from USER where nom_U = " + nom + "and mdp_U = " + mdp );
+		
+	}
+
+	int maxNumAthlete() throws SQLException{
+		this.st = this.laConnexion.createStatement();
+		ResultSet rs = this.st.executeQuery("select max(id_A) from ATHLETE");
+		if (rs.next()){
+			int maxnum = rs.getInt(1);
+			/** int macnum.rs.get("max(numJoueur)") */
+			return maxnum;
+		}
+		return -1;
+	}
+
+	
+	int insererAthlete( participant.Athlete j) throws  SQLException{
+		PreparedStatement ps = this.laConnexion.prepareStatement("insert into ATHLETE(id_A,nom_A,prenom_A,sexe_A,id_P,force_A,endurance,agilite,id_E) values (?,?,?,?,?,?,?,?,?)");
+		int nouvNum = maxNumAthlete()+1;
+		ps.setInt(1, nouvNum);
+		ps.setString(2, j.getNom());
+		ps.setString(3, j.getPrenom());
+		ps.setString(4, j.getSexe());
+		ps.setInt(5, j.getPays().getNum());
+		ps.setInt(6, j.getForce());
+		ps.setInt(7, j.getEndurance());
+		ps.setInt(8, j.getAgilite());
+		ps.setInt(9, j.getEquipe().getNum());
+		ps.executeUpdate();
+		return nouvNum;
+	}
+
+	void insererParticipation(Athlete a, Epreuve e)throws SQLException{
+		PreparedStatement ps = this.laConnexion.prepareStatement("insert into PARTICIPER(id_A,id_Ep) values (?,?)");
+		ps.setInt(1, getNumAthlete(a));
+		ps.setInt(2, getNumEpreuve(e));
+		ps.executeUpdate();
+	}
+
+	int getNumAthlete(Athlete a) throws SQLException{
+		this.st = this.laConnexion.createStatement();
+		ResultSet rs = this.st.executeQuery("SELECT id_A FROM ATHLETE WHERE nom_A = "+ a.getNom() +"AND prenom_A = "+ a.getPrenom()+"AND sexe_A =" + a.getSexe());
+			if (rs.next()){
+				int maxnum = rs.getInt(1);
+
+				return maxnum;
+			}
+			return -1;
+	}
+
+	int getNumEpreuve(Epreuve e)throws SQLException{
+
+		PreparedStatement ps = this.laConnexion.prepareStatement("SELECT id_Ep FROM EPREUVE WHERE nom = "+  e.getNom()+"AND categorie = "+ e.getCategorie()+ "AND id_S = " + e.getSport());
+		ResultSet rs = ps.executeQuery();
+			if (rs.next()){
+				int maxnum = rs.getInt(1);
+
+				return maxnum;
+			}
+			return -1;
+	}
+
+	
+	void majAthlete(ATHLETE a)throws SQLException{
+		PreparedStatement ps = this.laConnexion.prepareStatement("UPDATE ATHLETE SET nom_A = ?,prenom_A = ?, sexe_A=?,id_P=?,force_A=?,endurance=?, agilite = ?, id_E = ? where id_A = ?" );
+		ps.setString(1, a.getNom());
+		ps.setString(2, a.getPrenom());
+		ps.setString(3, a.getSexe());
+		ps.setString(4, a.getPays().getNum());
+		ps.setInt(5, a.getForce());
+		ps.setInt(6, a.getEndurance());
+		ps.setInt(7, a.getAgilite());
+		ps.setInt(8, a.getEquipe().getNum());
+		ps.setInt(9, a.getNum());
+		ps.executeUpdate();
+    	}
+
+	void insererUser(Utilisateur user) throws SQLException{
+		PreparedStatement ps = this.laConnexion.prepareStatement("insert into USER(nom_U,mdp_U,role_u) values (?,?,?)");
+		ps.setString(1, user.getNom());
+		ps.setString(2, user.getMdp());
+		ps.setString(3, "journaliste");
+		ps.executeUpdate();
+
+	}
+
+	void majUser(Utilisateur user)throws SQLException{
+		PreparedStatement ps = this.laConnexion.prepareStatement("UPDATE USER SET role_u = ? where nom_U = ? and mdp_U = ?" );
+		ps.setString(1, user.getRole());
+		ps.setString(2, user.getNom());
+		ps.setString(3, user.getMdp());
+		ps.executeUpdate();
+    	}
+
+	void lancerEpreuve(ComparateurMedailleOr comp, Tri tri, Epreuve e){
+		PreparedStatement ps = this.laConnexion.prepareStatement("SELECT id_A,nom_A,prenom_A,sexe_A,id_P,force_A,endurance,agilite,id_E from ATHLETE natural join PARTICIPER natural join EPREUVE where id_Ep =" + e.getNum());
+		ResultSet rs = ps.executeQuery();
+			while (rs.next()){
+				Integer num = 
+			}
 	}
 
 
